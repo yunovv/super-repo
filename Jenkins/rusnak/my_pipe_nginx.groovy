42374abd27c1a2e6dc631f695f96e9f6e5d4d7b2
root <root@HOME-PC.localdomain>
2022-04-03 12:11:59 +0300

    Added pipe in Jenkins/rusnak/

---
+++ Jenkins/rusnak/my_pipe_nginx.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/env groovy
+
+def git_cred_id = 'rusnak_git'
+def server_cred_id = 'servers_auth'
+def server_ip = '192.168.2.21'
+def server_user = 'admin'
+def git_repo = 'git@github.com:yunovv/super-repo.git'
+
+
+pipeline {
+    agent any
+    parameters {
+        booleanParam(name: 'show_debug_info', defaultValue: false)
+        booleanParam(name: 'deploy_nginx', defaultValue: false)
+        //text(name: 'input_text_field', description: 'input text field')
+    }
+	
+    stages {
+        stage('Checkout Scripts Repo') {
+            steps {
+                script {
+                    // работаем с git при помощи плагина jenkins
+                    sh 'mkdir super_repo'
+                    dir("super_repo") {
+                        checkout([$class: 'GitSCM', branches: [[name: '*/rusnak_jenkins']],userRemoteConfigs: [[url: git_repo, credentialsId: git_cred_id]]])
+						sh 'ls -l'
+                    }
+                }
+            }
+        }	
+	
+	
+	
+	
+
+
+    post {
+        cleanup {
+            cleanWs()
+        }
+    }
+}
