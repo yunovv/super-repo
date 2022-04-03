bd8000cb39128068d258135166145d07e6883add
root <root@HOME-PC.localdomain>
2022-04-03 12:47:31 +0300

    did some changes pipe #9

--- Jenkins/rusnak/my_pipe_nginx.groovy
+++ Jenkins/rusnak/my_pipe_nginx.groovy
@@ -2,7 +2,7 @@
 
 def git_cred_id = 'rusnak_git'
 def server_cred_id = 'servers_auth'
-def server_ip = '192.168.2.21'
+//def server_ip = '192.168.2.21'
 def server_user = 'admin'
 def git_repo = 'git@github.com:yunovv/super-repo.git'
 
@@ -10,9 +10,9 @@ def git_repo = 'git@github.com:yunovv/super-repo.git'
 pipeline {
     agent any
     parameters {
-        booleanParam(name: 'show_debug_info', defaultValue: false)
-        booleanParam(name: 'deploy_nginx', defaultValue: false)
-        //text(name: 'input_text_field', description: 'input text field')
+        //booleanParam(name: 'show_debug_info', defaultValue: false)
+        //booleanParam(name: 'deploy_nginx', defaultValue: false)
+        text(name: 'server_ip', description: 'Input Server IP-address.' , defaultValue: '192.168.2.21')
     }
 	
 	stages {
@@ -22,9 +22,7 @@ pipeline {
 					sh "pwd"
 					sh "ls -l"
 					dir("ansible/rusnak") {
-						sh "cat hosts"
 						sh "echo ${server_ip} > hosts"
-						sh "cat hosts"
 					}
                 }
             }
